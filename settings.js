4baa637a1053242de88d465128c3a155ae1754bd
opvicky-hub <godswillvictor742@gmail.com>
2025-01-01 11:51:17 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "opay"
 
-global.banknumber = "8133729715"
+global.banknumber = "8039583975"
 
-global.bankowner = "FADARE"
+global.bankowner = "larry"
 
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.wlcm = true
 global.autoblockmorroco = true //auto block 212 (true to on, false to off)
 global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
